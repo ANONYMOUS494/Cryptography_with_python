54a01aa42b3ffa7d36c9a15ac83a7b5f12b7fd6e
ANONYMOUS494 <58910756+ANONYMOUS494@users.noreply.github.com>
2020-10-11 21:54:12 +0530

    Create reverse-cipher.py

---
+++ reverse-cipher.py
@@ -0,0 +1,9 @@
+message = input(" enter your message:")
+translated =' '  #cipher text is stored in this variable
+i = len(message) - 1
+while i>=0:
+    translated = translated + message[i]
+    i = i - 1
+    print("The cipher text is : ", translated)
+
+ 
